dd136f880ab54cd8f782993727771d45ec89b721
Alex-Devoid <39149715+Alex-Devoid@users.noreply.github.com>
2024-07-16 00:20:05 -0400

    Share analysis, ignore large files

---
+++ commit.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+# git add mountains; git commit -m "mountains";git push -u origin master
+
+# git add min_max_temp/CPC_Global_Daily_Temperature/tmin; git commit -m "min_max_temp1";git push origin master
+# git add min_max_temp/CPC_Global_Daily_Temperature/tmin; git commit -m "min_max_temp2";git push origin master
+
+# git add bp_stats; git commit -m "bp_stats";git push origin master
+
+# git add deaths_in_cell_service/voice_2020_6; git commit -m "deaths_in_cell_service1";git push -u origin master
+# git add deaths_in_cell_service/Call\ service\ 2020\ on\ all\ deaths.ipynb; git commit -m "deaths_in_cell_service2";git push -u origin master
+# git add deaths_in_cell_service/deaths_cell_coverage.csv; git commit -m "deaths_in_cell_service3";git push -u origin master
+
+# git add empty_border_by_year/corridors; git commit -m "empty_border_by_year1";git push -u origin master
+# git add empty_border_by_year/scratch; git commit -m "empty_border_by_year2";git push -u origin master
+# git add empty_border_by_year/empty_border_by_year_by_corridor.ipynb; git commit -m "empty_border_by_year3";git push -u origin master
+# git add empty_border_by_year/merge_border_wall; git commit -m "empty_border_by_year4";git push -u origin master
+# git add empty_border_by_year/empty_border_cor_year_1990.csv; git commit -m "empty_border_by_year5";git push -u origin master
+
+# git add deaths_in_cell_service; git commit -m "deaths_in_cell_service";git push -u origin master
+
+git add distance_from_roads_towns_border/towns/cities_towns.geojson; git commit -m "cities_towns.geojson";git push -u origin master
+git add distance_from_roads_towns_border/roads/roads.geojson; git commit -m "roads.geojson";git push -u origin master
+# git add distance_from_roads_towns_border/roads; git commit -m "distance_from_roads_towns_border2";git push -u origin master
+# git add distance_from_roads_towns_border/border; git commit -m "distance_from_roads_towns_border3";git push -u origin master
+
+# git add border_sectors; git commit -m "border_sectors";git push -u origin master
+
+# git add altar_valley; git commit -m "altar_valley";git push -u origin master
+
+# git add death_files; git commit -m "death_files";git push -u origin master
+
+# git add .ipynb_checkpoints; git commit -m ".ipynb_checkpoints";git push -u origin master
+
+# git add all_datapoints_for_each_death.csv; git commit -m "all_datapoints_for_each_death.csv";git push -u origin master
+#
+# git add combine_datapoints.ipynb; git commit -m "combine_datapoints.ipynb";git push -u origin master
+#
+# git add combine_yuma_and_pima_deaths_files.ipynb; git commit -m "combine_yuma_and_pima_deaths_files.ipynb";git push -u origin master
+#
+# git add compare_negbin_quassi_poisson.Rmd; git commit -m "compare_negbin_quassi_poisson.Rmd";git push -u origin master
+#
+# git add death_datapoints_merged.csv; git commit -m "death_datapoints_merged.csv";git push -u origin master
+#
+# git add final\ analysis.ipynb; git commit -m "final analysis";git push -u origin master
+#
+# git add mean_death_datapoints.csv; git commit -m "mean_death_datapoints.csv";git push -u origin master
+#
+# git add mean_median_death_datapoints.csv; git commit -m "mean_median_death_datapoints.csv";git push -u origin master
+#
+# git add README.md; git commit -m "README.md";git push -u origin master
+
+# for d in ../deaths_in_cell_service/ALL_F477_2020_06_Mobile_Voice_Deployment-by_Provider ; do
+#     echo "$d"
+# done
